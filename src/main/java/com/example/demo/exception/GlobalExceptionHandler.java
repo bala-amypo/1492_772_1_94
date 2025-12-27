7f2d35f23d5c021b84bc110908e83fb33e8ee602
bala-amypo <bala@amypo.in>
2025-12-27 05:52:00 +0000

    User commit

---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.demo.exception;
+
+import com.example.demo.dto.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ApiResponse<Object>> handleBadRequest(
+            BadRequestException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ApiResponse<>(false, ex.getMessage(), null));
+    }
+}
